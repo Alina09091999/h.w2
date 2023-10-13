a04162d9eb09522f6e122ee91e25edc39ccbc6a0
Alina <alina09091999@yandex.ru>
2023-10-13 19:52:59 +0300

    Add file2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initially = 50;
-        int replenishment = 70;
+        int initially = 100;
+        int replenishment = 300;
         int bonus = replenishment / 100;
         int result = initially + replenishment + bonus;
+        int resultWithoutBonus = initially + replenishment;
 
         if (replenishment > 1000) {
             System.out.println("Итоговый счет " + result);
             System.out.println("Бонус " + bonus);
         } else {
-            System.out.println("Итоговый счет " + result);
+            System.out.println("Итоговый счет " + resultWithoutBonus);
             System.out.println("Бонус 0");
         }
     }
